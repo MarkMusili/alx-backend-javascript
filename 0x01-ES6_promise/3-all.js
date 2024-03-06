2c39e986c430254f2ff06519f7133d7a0a93060a
MarkMusili <musiliyrn@gmail.com>
2024-03-06 22:00:05 +0300

    Syntax change

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup() {
 
   Promise.all([uploadResponse, createResponse])
     .then((responses) => {
-      console.log(responses[0].body, responses[1].firstName, responses[1].lastName);
+      console.log(`${responses[0].body}, ${responses[1].firstName}, ${responses[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
