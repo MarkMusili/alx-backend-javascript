1028833f0295bce4bd9a6bae4ee7ea582218c321
MarkMusili <musiliyrn@gmail.com>
2024-03-06 17:26:42 +0300

    New version

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const uploadResponse = uploadPhoto(fileName);
-  const signUpResponse = signUpUser(firstName, lastName);
+  const uploadResponse = uploadPhoto(fileName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: (error) }));
 
-  return Promise.all([uploadResponse, signUpResponse])
-    .then((responses) => {
-      console.log(responses[0].firstName);
-    })
-    .catch((err) => console.error(err));
+  const signUpResponse = signUpUser(firstName, lastName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: error }));
+
+  return Promise.all([uploadResponse, signUpResponse]);
 }
