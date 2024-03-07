93d8efa5c12ed5dc9e0ca54e7d65b99db67a86d6
MarkMusili <musiliyrn@gmail.com>
2024-03-07 16:35:05 +0300

    Syntax error

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const ClassRoom10 = new ClassRoom(10);
   const ClassRoom20 = new ClassRoom(20);
-  const ClassRoom30 = new ClassRoom(30);
+  const ClassRoom34 = new ClassRoom(34);
 
-  return [ClassRoom10, ClassRoom20, ClassRoom30];
+  return [ClassRoom10, ClassRoom20, ClassRoom34];
 }
