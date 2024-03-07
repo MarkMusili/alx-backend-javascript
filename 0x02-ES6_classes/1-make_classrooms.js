6c7b50d9843a42bc6da89a56d70d69db783eb3d2
MarkMusili <musiliyrn@gmail.com>
2024-03-07 08:43:51 +0300

    Array of classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const ClassRoom10 = new ClassRoom(10);
+  const ClassRoom20 = new ClassRoom(20);
+  const ClassRoom30 = new ClassRoom(30);
+
+  return [ClassRoom10, ClassRoom20, ClassRoom30];
+}
