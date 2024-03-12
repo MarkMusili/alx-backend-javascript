89cf0ee1f3d220100d17a5473f26169a591b69c6
MarkMusili <musiliyrn@gmail.com>
2024-03-12 14:13:05 +0300

    If else clause approach

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    const buffer = new ArrayBuffer(length);
-    const int8View = new Int8Array(buffer);
-    int8View[position] = value;
-    const view = new DataView(buffer);
-    return view;
-  } catch (e) {
-    throw Error('Position outside range');
-  }
+  if (position >= length) throw Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+  const view = new DataView(buffer);
+  return view;
 }
