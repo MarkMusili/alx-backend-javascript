e2b91d1814c460dd3885b308c86a9e68f8ae1c34
MarkMusili <musiliyrn@gmail.com>
2024-03-14 21:07:55 +0300

    Pycodestyle validation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Type-annotated function add that takes a 
+Type-annotated function add that takes a
 float a and a float b as arguments
 """
 
+
 def add(a: float, b: float) -> float:
-    """"Adds two floats 
+    """"Adds two floats
     and returns a float"""
     return a + b
-
