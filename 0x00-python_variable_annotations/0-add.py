f54211476513b9365f76855526abe882fadd4e1b
MarkMusili <musiliyrn@gmail.com>
2024-03-14 21:05:07 +0300

    Documentation and pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """
-Type-annotated function add that takes a float a and a float b as arguments
+Type-annotated function add that takes a 
+float a and a float b as arguments
 """
 
 def add(a: float, b: float) -> float:
-    """"Adds two floats and returns a float"""
+    """"Adds two floats 
+    and returns a float"""
     return a + b
+
