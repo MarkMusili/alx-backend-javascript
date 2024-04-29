6e7a2c07c28c546e5a54f5caece7b52b14e976e7
MarkMusili <musiliyrn@gmail.com>
2024-04-29 22:01:51 +0300

    Es lint fixes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,42 +1,40 @@
-const { createServer } = require('http')
+const { createServer } = require('http');
+const process = require('process');
 const countStudents = require('./3-read_file_async');
-const process = require('process')
-
 
 const path = process.argv[2] || 'database.csv';
-const port = 1245
+const port = 1245;
 const hostname = '127.0.0.1';
 
-const app = createServer((req, res) => {
-    if (req.method == 'GET' && req.url == '/'){
-        homeHandler(req, res);
-    };
-    if (req.method == 'GET' && req.url == '/students'){
-        studentsHandler(req, res, path);
-    };
-});
-
-
-function homeHandler(req, res){
-    res.statuscode = 200
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello Holberton School!");
+function homeHandler(req, res) {
+  res.statuscode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 }
 
-function studentsHandler(req, res, path){
-    res.statuscode = 200
-    res.setHeader('Content-Type', 'text/plain');
-    res.write("This is the list of our students");
-    countStudents(path)
-        .then((students) => {
-            res.write(students);
-            res.end()
-        })
-        .catch((error) => {
-            res.write(error.message);
-            res.end()
-        });
-    };
+function studentsHandler(req, res, path) {
+  res.statuscode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students');
+  countStudents(path)
+    .then((students) => {
+      res.write(students);
+      res.end();
+    })
+    .catch((error) => {
+      res.write(error.message);
+      res.end();
+    });
+}
 
-app.listen(port, hostname)
-module.exports = app
+const app = createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    homeHandler(req, res);
+  }
+  if (req.method === 'GET' && req.url === '/students') {
+    studentsHandler(req, res, path);
+  }
+});
+
+app.listen(port, hostname);
+module.exports = app;
