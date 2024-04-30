5039fe841310eb32a708126b51f71fcd4fe824b4
MarkMusili <musiliyrn@gmail.com>
2024-04-30 12:30:38 +0300

    Syntax logical errror fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
-  r1.close();
+  r1.close(); //Fixing this later
 });
 
 rl.on('close', () => {
